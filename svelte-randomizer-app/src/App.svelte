4a783caff2d96954aedc0c922dcc62032f1ecc1e
Spenser <spenser@minddnd.com>
2020-09-10 14:28:19 -0400

    Add Project

---
+++ svelte-randomizer-app/src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+import Randomizer from './Randomizer.svelte';
+	let list=[];
+	let newItem =''
+
+	const addItem = (e) => {
+		e.preventDefault();
+		if(newItem != ''){
+			list.push({item: newItem, id: new Date().getTime()});
+			newItem = '';
+		}
+	}
+</script>
+
+<main>
+<h1>Randomize Stuff</h1>
+<Randomizer list={list}/>
+<div class="add-box">
+	<form>
+		<input type="text" bind:value={newItem} placeholder="Add New Item">
+		<button class="btn" type="submit" on:click={addItem}>Add+</button>
+	</form>
+</div>
+<div class="list-box">
+
+	<ul>
+		{#each list as item, i (item.id)}
+			<li>{i + 1} - d{item.item}</li>
+		{/each}
+	</ul>
+</div>
+
+</main>
+
---
+++ svelte-randomizer-app/src/Randomizer.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let list;
+  let chosen = 'Choose';
+
+  const choose = (e) => {
+    e.preventDefault();
+    chosen = list[Math.floor(Math.random() * list.length)].item.toUpperCase();
+  }
+</script>
+<div class="randomize-box">
+  <p class="chosen">{chosen}</p>
+
+  <button class="btn" on:click={choose}>Choose</button> 
+</div>
